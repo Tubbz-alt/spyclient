267398b4f6854276bbdb52697bd5e55dea7dc546
Sam <sam210723@hotmail.com>
2020-01-13 20:42:41 +1000

    Socket properties

--- spyclient/spyclient.py
+++ spyclient/spyclient.py
@@ -11,8 +11,8 @@ from . import enums, tuples
 ## Constants
 PACKAGE_VER = "1.0"             # Python package version
 PROTOCOL_VER = 0x2000638        # Protocol implementation version
-DEF_HOST = "127.0.0.1"          # Default socket host
-DEF_PORT = 5555                 # Default socket port
+DEFAULT_HOST = "127.0.0.1"      # Default socket host
+DEFAULT_PORT = 5555             # Default socket port
 TIMEOUT = 2                     # Socket read timeout (sec)
 
 
@@ -23,5 +23,7 @@ class SpyClient:
     :param port: SpyServer TCP port (5555)
     """
 
-    def __init__(self, host=DEF_HOST, port=DEF_PORT):
-        return
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.host = host
+        self.port = port
+        self.name = f"SpyClient for Python v{PACKAGE_VER}"
--- test/test.py
+++ test/test.py
@@ -1,7 +1,13 @@
 from spyclient import SpyClient
 
 def init():
+    # Create SpyClient instance
     c = SpyClient("127.0.0.1", 5555)
+
+    # Print properties
+    print(f"HOST IP:      {c.host}")
+    print(f"HOST PORT:    {c.port}")
+    print(f"CLIENT NAME:  {c.name}")
 
 
 try:
