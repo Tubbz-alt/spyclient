a3a1afb409998593247c0490d588cfa919f915db
Sam <sam210723@hotmail.com>
2020-01-13 21:33:20 +1000

    Send HELLO command

--- spyclient/spyclient.py
+++ spyclient/spyclient.py
@@ -6,11 +6,12 @@ Airspy SpyServer client implementation for Python 3
 """
 
 import socket
+import struct
 from . import enums, tuples
 
 ## Constants
 PACKAGE_VER = "1.0"             # Python package version
-PROTOCOL_VER = 0x2000638        # Protocol implementation version
+PROTOCOL_VER = 0x20006A4        # Protocol implementation version
 DEFAULT_HOST = "127.0.0.1"      # Default socket host
 DEFAULT_PORT = 5555             # Default socket port
 TIMEOUT = 2                     # Socket read timeout (sec)
@@ -35,6 +36,39 @@ class SpyClient:
         self.connected = False
 
 
+    #region Protocol Functions
+    def say_hello(self):
+        """
+        Say hello to server
+        """
+
+        # Protocol version as unsigned little-endian integer
+        ver = struct.pack('<I', PROTOCOL_VER)
+
+        # Get bytes from client name
+        client = self.name.encode('UTF-8')
+
+        # Send version and client name to server
+        body = ver + client        
+        self.send_command(enums.CommandType.HELLO.value, body)
+    
+    def send_command(self, cmd, body):
+        """
+        Sends command packet to server
+        :param cmd: Command type
+        :param body: Command body bytes
+        """
+
+        # Assemble command header
+        length = struct.pack('<I', len(body))
+        header = cmd + length
+
+        # Send command to server
+        command = header + body
+        self.send(command)
+    #endregion
+
+
     #region Socket Functions
     def connect(self):
         """
@@ -52,6 +86,9 @@ class SpyClient:
             raise
         
         self.connected = True
+
+        self.say_hello()
+
         return True
 
     def disconnect(self):
