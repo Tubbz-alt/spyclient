1016e1d352b96d055dde5ddc0437b1e72117645b
Sam <sam210723@hotmail.com>
2020-01-13 21:32:35 +1000

    Add enums

--- spyclient/enums.py
+++ spyclient/enums.py
@@ -1,1 +1,58 @@
+import struct
 from enum import Enum
+
+
+class CommandType(Enum):
+    """
+    Command types
+    """
+
+    HELLO = struct.pack('<I', 0)   # Say hello to server
+    GET   = struct.pack('<I', 1)   # Get setting from server
+    SET   = struct.pack('<I', 2)   # Set setting on server
+    PING  = struct.pack('<I', 3)   # Check server is alive
+
+class MessageType(Enum):
+    """
+    Message types
+    """
+
+    DEVICE_INFO  = 0
+    CLIENT_SYNC  = 1
+    PONG         = 2
+    READ_SETTING = 3
+    UINT8_IQ     = 100
+    INT16_IQ     = 101
+    INT24_IQ     = 102
+    FLOAT_IQ     = 103
+    UINT8_AF     = 200
+    INT16_AF     = 201
+    INT24_AF     = 202
+    FLOAT_AF     = 203
+    DINT4_FFT    = 300
+    UINT8_FFT    = 301
+
+class DeviceType(Enum):
+    """
+    Device types
+    """
+
+    INVALID    = 0
+    AIRSPY_ONE = 1
+    AIRSPY_HF  = 2
+    RTLSDR     = 3
+
+class SettingType(Enum):
+    STREAMING_MODE     = struct.pack('<I', 0)
+    STREAMING_ENABLED  = struct.pack('<I', 1)
+    GAIN               = struct.pack('<I', 2)
+    IQ_FORMAT          = struct.pack('<I', 100)
+    IQ_FREQUENCY       = struct.pack('<I', 101)
+    IQ_DECIMATION      = struct.pack('<I', 102)
+    IQ_DIGITAL_GAIN    = struct.pack('<I', 103)
+    FFT_FORMAT         = struct.pack('<I', 200)
+    FFT_FREQUENCY      = struct.pack('<I', 201)
+    FFT_DECIMATION     = struct.pack('<I', 202)
+    FFT_DB_OFFSET      = struct.pack('<I', 203)
+    FFT_DB_RANGE       = struct.pack('<I', 204)
+    FFT_DISPLAY_PIXELS = struct.pack('<I', 205)
