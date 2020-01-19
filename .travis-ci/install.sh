3b47bae83c623441643185f57908b27b39fc7751
Sam <sam210723@hotmail.com>
2020-01-19 22:23:46 +1000

    Simplify Travis CI job

--- .travis-ci/install.sh
+++
@@ -1,9 +0,0 @@
-# Download latest SpyServer
-wget https://airspy.com/downloads/spyserver-linux-x64.tgz
-tar -xvzf spyserver-linux-x64.tgz
-chmod +x ./spyserver
-./spyserver &
-
-# Build and install package
-#pip install -r requirements.txt
-make install
