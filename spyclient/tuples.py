b7c4fb95a8247c35f8b93a41041875e4f8c9854f
Sam <sam210723@hotmail.com>
2020-01-19 12:23:33 +1000

    Protocol structs

--- spyclient/tuples.py
+++ spyclient/tuples.py
@@ -1,1 +1,54 @@
 from typing import NamedTuple
+
+class MessageHeader(NamedTuple):
+    """
+    Message header struct
+    """
+
+    protocol:     int
+    message_type: int
+    stream_type:  int
+    sequence:     int
+    size:         int
+
+class ProtocolVersion(NamedTuple):
+    """
+    Server protocol version struct
+    """
+
+    major: int
+    minor: int
+    patch: int
+
+class DeviceInfo(NamedTuple):
+    """
+    Device information struct
+    """
+
+    device_type:       int
+    serial:            int
+    max_sample_rate:   int
+    max_bandwidth:     int
+    decimation_stages: int
+    gain_stages:       int
+    max_gain:          int
+    min_frequency:     int
+    max_frequency:     int
+    adc_resolution:    int
+    min_iq_decimation: int
+    forced_iq_format:  int
+
+class ClientSync(NamedTuple):
+    """
+    Client sync struct
+    """
+
+    can_control: int
+    gain:        int
+    device_cf:   int
+    iq_cf:       int
+    fft_cf:      int
+    min_iq_cf:   int
+    max_iq_cf:   int
+    min_fft_cf:  int
+    max_fft_cf:  int
